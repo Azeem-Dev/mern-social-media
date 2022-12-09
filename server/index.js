9c213a02447cbad84bdb8f1f304fac617d2a0371
Azeem Sharif <72242160+Azeem-Dev@users.noreply.github.com>
2022-12-09 13:08:43 +0500

    added multer to save in out static server storage

--- server/index.js
+++ server/index.js
@@ -24,3 +24,20 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+// FILE STORAGE
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/assets");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+// MONGO DATABASE
+
+
