ca1a6b29fd23798bdc460c16a14cd055361f6732
Mian Muhammad Azeem <expmias@outlook.com>
2022-12-12 19:23:25 +0500

    some new routes added

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth.js";
-import { createPost,getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import {
+  createPost,
+  getFeedPosts,
+  getUserPosts,
+  likePost,
+} from "../controllers/posts.js";
 import upload from "../uploadSetup.js";
 
 const router = express.Router();
 
-router.post("/", verifyToken, upload.single("picture"),createPost);
+router.post("/", verifyToken, upload.single("picture"), createPost);
 router.get("/", verifyToken, getFeedPosts);
 router.get("/:userId/posts", verifyToken, getUserPosts);
 router.patch("/:id/like", verifyToken, likePost);
