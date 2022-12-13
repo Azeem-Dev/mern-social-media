32c056a77326f42713594219073db6c8baea5ce2
Mian Muhammad Azeem <expmias@outlook.com>
2022-12-13 13:31:09 +0500

    done with backend apis

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -25,6 +25,40 @@ export const createPost = async (req, res) => {
     res.status(409).json({ msg: err.message });
   }
 };
-export const getFeedPosts = async (req, res) => {};
-export const getUserPosts = async (req, res) => {};
-export const likePost = async (req, res) => {};
+export const getFeedPosts = async (req, res) => {
+  try {
+    const post = await Post.find();
+    res.status(201).json(post);
+  } catch (err) {
+    res.status(404).json({ msg: err.message });
+  }
+};
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const posts = await Post.find({ userId });
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(404).json({ msg: err.message });
+  }
+};
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+
+    const post = await Post.findById(id);
+    const isLiked = post.likes.get(userId);
+    if (isLiked) post.likes.delete(userId);
+    else post.likes.set(userId, true);
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { likes: post.likes },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(404).json({ msg: err.message });
+  }
+};
